c4f9a391a0a8590bc5be1f96129fee3415bac878
nameerakhter <akhtarnameer@gmail.com>
2024-06-22 21:51:37 +0530

    signingUp user and custom OTP

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,26 @@
+import { resend } from "@/lib/resend";
+
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "../../types/ApiResponse";
+import { string } from "zod";
+import { Console } from "console";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+):Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Annonymouse Feedback | Verification Code',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+        return{sucess: true, message: "Verificaiton email sent"}
+    } catch (emailError) {
+        console.error("error sending Verification email", emailError);
+        return{sucess: false, message: "Failed to send Verification email"}
+        
+    }
+}
