10c837568ce3fd6a3790cfda55775e993b6ea02a
nameerakhter <akhtarnameer@gmail.com>
2024-07-03 17:18:46 +0530

    implemented front-end of sign-up

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,15 +1,9 @@
-'use client'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import Link from "next/link"
-
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      signin
     </div>
   )
 }
