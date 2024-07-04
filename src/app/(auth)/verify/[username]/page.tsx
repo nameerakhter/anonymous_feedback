d1e0da12c8f88bfe68c970f6b7da64c076028d55
nameerakhter <akhtarnameer@gmail.com>
2024-07-04 13:54:19 +0530

    working on user verification

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const VerifyAccount = () => {
+    const router = useRouter()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VerifyAccount
